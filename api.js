73ec0b37c7b7b647b5b8c25ffd3a4351c5ecb873
Matt Karl <matt@mattkarl.com>
2014-01-26 14:10:04 -0500

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Event",
+        "EventDispatcher"
+    ],
+    "modules": [
+        "Canteen_Events"
+    ],
+    "allModules": [
+        {
+            "displayName": "Canteen\\Events",
+            "name": "Canteen_Events"
+        }
+    ]
+} };
+});
